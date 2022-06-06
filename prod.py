b889fc3f520227aac5f5964716faa284d1d85f56
Umidjon <mrumidjon2020@gmail.com>
2022-06-06 20:44:16 +0500

    Returns the product of all numbers in the array

--- prod.py
+++ prod.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def arr_product(arr: np.ndarray) -> int:
     """
     Returns the product of all numbers in the array
@@ -8,3 +9,10 @@ def arr_product(arr: np.ndarray) -> int:
     Returns:
         int: product of all numbers
     """
+    return arr.prod()
+
+
+# arr = np.array([[3, 4],
+#                 [1, 5],
+#                 [3, 2]])
+# print(arr_product(arr))
